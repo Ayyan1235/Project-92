f1c08497f67e3e4af9e45f128ff55473107d258b
Ayyan1235 <139350156+Ayyan1235@users.noreply.github.com>
2023-10-25 17:45:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function new_image()
 {
   fabric.Image.fromURL('Bd.jpg', function(Img) {
     block_image_object = Img;
-    block_image_object.scaletoWidth(700);
+    block_image_object.scaleToWidth(700);
     block_image_object.scaleToHeight(510);
     block_image_object.set({
         top:0,
